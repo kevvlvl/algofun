361c3e6ad61a3cd5d22ab8c3a2d730602eec51f9
Kev <ksayad@gmail.com>
2023-09-08 17:26:47 -0400

    more

--- calc/calc.go
+++ calc/calc.go
@@ -1,6 +1,11 @@
 package calc
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 // TwoSum returns the indexes of the two numbers that give us the target summed value
 func TwoSum(nums []int, target int) []int {
@@ -19,4 +24,84 @@ func TwoSum(nums []int, target int) []int {
 	}
 
 	return []int{}
+}
+
+// IsPalindrome returns true if x reads as a palindrome
+func IsPalindrome(x int) bool {
+
+	s := strconv.Itoa(x)
+	l := len(s) - 1
+
+	sb := []byte(s)
+	sbr := []byte(s)
+
+	for i := 0; i <= l; i++ {
+		sbr[i] = sb[l-i]
+	}
+
+	return bytes.Equal(sb, sbr)
+}
+
+// RomanToInteger return the int value of the roman numeral string
+func RomanToInteger(r string) int {
+
+	value := 0
+
+	for i := 0; i < len(r); i++ {
+		if string(r[i]) == "M" {
+			value += 1000
+		}
+
+		if string(r[i]) == "D" {
+			value += 500
+		}
+
+		if string(r[i]) == "C" {
+			value += 100
+		}
+
+		if string(r[i]) == "L" {
+			value += 50
+		}
+
+		if string(r[i]) == "X" {
+			value += 10
+		}
+
+		if string(r[i]) == "V" {
+			value += 5
+		}
+
+		if string(r[i]) == "I" {
+			value += 1
+		}
+	}
+
+	if strings.Index(r, "IV") > -1 {
+		value -= 2
+	}
+
+	if strings.Index(r, "IX") > -1 {
+		value -= 2
+	}
+
+	if strings.Index(r, "XL") > -1 {
+		value -= 20
+	}
+
+	if strings.Index(r, "XC") > -1 {
+		value -= 20
+	}
+
+	if strings.Index(r, "CD") > -1 {
+		value -= 200
+	}
+
+	if strings.Index(r, "CM") > -1 {
+		value -= 200
+	}
+
+	fmt.Printf("Value of %v is %v\n", r, value)
+
+	return value
 }
--- main.go
+++ main.go
@@ -10,14 +10,28 @@ func main() {
 
 	nums1 := []int{2, 4, 9, 2, 1, 10, 29}
 
+	fmt.Println("------------------------ Find Numbers that sum the target")
 	fmt.Println(calc.TwoSum(nums1, 11))
 	fmt.Println(calc.TwoSum(nums1, 39))
 	fmt.Println(calc.TwoSum(nums1, 13))
 
+	fmt.Println("------------------------ Valid Parenthesis")
 	fmt.Println(strs.IsValid("()"))
 	fmt.Println(strs.IsValid("(){}[]"))
 	fmt.Println(strs.IsValid("((){}[])"))
 	fmt.Println(strs.IsValid("(]"))
 	fmt.Println(strs.IsValid("({{)"))
 	fmt.Println(strs.IsValid("{[]}"))
+
+	fmt.Println("------------------------ Palindromes")
+	fmt.Println(calc.IsPalindrome(101))
+	fmt.Println(calc.IsPalindrome(50))
+	fmt.Println(calc.IsPalindrome(55))
+	fmt.Println(calc.IsPalindrome(225522))
+	fmt.Println(calc.IsPalindrome(-50))
+
+	fmt.Println("------------------------ Roman numerals converter")
+	calc.RomanToInteger("III")
+	calc.RomanToInteger("LVIII")
+	calc.RomanToInteger("MCMXCIV")
 }
